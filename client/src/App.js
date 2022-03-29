2b891db8dee090623edcee5d6d6386e367004773
Bhuwan <bhuwankumar2000@gmail.com>
2022-03-29 16:56:51 +0530

    university list added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './layouts/Header';
 import Intro from './components/Intro';
+import University from './components/University';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Intro />
+      <University />
     </div>
   );
 }
---
+++ client/src/components/University.jsx
@@ -0,0 +1,117 @@
+import './university.css'
+
+const University = () => {
+	return (
+		<div className='uni'>
+			<div className='uni-header'>
+				Top university for MS in Computer Science in the USA
+			</div>
+			<div className='uni-body'>
+				Here is a list of the top ten universities in the USA according to the Times Higher Education (THE) World Rankings 2020 for computer science courses. As per THE, the universities are ranked based on their teaching style, international outlook, industry income, and research facilities.
+			</div>
+			<ul className='uni-list'>
+				<li className='item' >
+					University
+				</li>
+				<li className='item' >
+					Ranking
+				</li>
+				<li className='item' >
+					FEE
+				</li>
+				<li className='item' >
+					Stanford University
+				</li>
+				<li className='item' >
+					1
+				</li>
+				<li className='item' >
+					$55,890
+				</li>
+				<li className='item' >
+					Massachusetts Institute of Technology
+				</li>
+				<li className='item' >
+					2
+				</li>
+				<li className='item' >
+					$43,210
+				</li>
+				<li className='item' >
+					Carnegie Melon University
+				</li>
+				<li className='item' >
+					3
+				</li>
+				<li className='item' >
+					$98,000
+				</li>
+				<li className='item' >
+					Harvard University
+				</li>
+				<li className='item' >
+					4
+				</li>
+				<li className='item' >
+					$43,296
+				</li>
+				<li className='item' >
+					Princeton University
+				</li>
+				<li className='item' >
+					5
+				</li>
+				<li className='item' >
+					$1,07,540
+				</li>
+				<li className='item' >
+					California Institute of Technology
+				</li>
+				<li className='item' >
+					6
+				</li>
+				<li className='item' >
+					$41,408
+				</li>
+				<li className='item' >
+					University of California, Los Angeles
+				</li>
+				<li className='item' >
+					7
+				</li>
+				<li className='item' >
+					$63,898
+				</li>
+				<li className='item' >
+					Cornell University
+				</li>
+				<li className='item' >
+					8
+				</li>
+				<li className='item' >
+					$59,000
+				</li>
+				<li className='item' >
+					Georgia Institute of Technology
+				</li>
+				<li className='item' >
+					9
+				</li>
+				<li className='item' >
+					$62,740
+				</li>
+				<li className='item' >
+					Columbia University
+				</li>
+				<li className='item' >
+					10
+				</li>
+				<li className='item' >
+					$63,120
+				</li>
+			</ul>
+		</div>
+	)
+}
+
+export default University
